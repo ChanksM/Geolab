d6f1f09c8f756a77d3caf2d2b4f9f1db1aaf664f
ChanksM <45490290+ChanksM@users.noreply.github.com>
2019-02-10 22:40:35 +0400

    sircxvili
    aba aba

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,44 @@
+// 'use strict';
+ 
+// var gulp = require('gulp');
+// var sass = require('gulp-sass');
+ 
+// sass.compiler = require('node-sass');
+ 
+// gulp.task('sass', function () {
+//   return gulp.src('scss/style.scss')
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(gulp.dest('./styles'));
+// });
+ 
+// gulp.task('watch', function () {
+//   gulp.watch('scss/style.scss', gulp.parallel('sass'));
+// });
+
+
+
+
+
+
+
+
+
 'use strict';
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
+const autoprefixer = require('gulp-autoprefixer');
+// const cleanCSS = require('gulp-clean-css');
 sass.compiler = require('node-sass');
  
 gulp.task('sass', function () {
   return gulp.src('scss/style.scss')
     .pipe(sass().on('error', sass.logError))
+    // .pipe(cleanCSS())
+    .pipe(autoprefixer({
+      browsers: ['last 10 versions'],
+      cascade: false
+  }))
     .pipe(gulp.dest('./styles'));
 });
  
